bd209af36840084604bb5f30759617a5915b5f2d
AnirveshArcot <arcotanirvesh@gmail.com>
2024-08-23 18:05:50 +0530

    ngrok add

--- src/setupEnv.jsx
+++ src/setupEnv.jsx
@@ -1,1 +1,1 @@
-export const VITE_API_URL = " https://19da-2401-4900-9006-2819-8cda-95a5-9b9a-dd68.ngrok-free.app";
+export const VITE_API_URL = " https://4acf-2401-4900-9006-2819-8cda-95a5-9b9a-dd68.ngrok-free.app";
